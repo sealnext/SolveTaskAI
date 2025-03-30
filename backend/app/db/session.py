b45999ac5dc82ea8627be4ba47726c603ddbf176
Andrei Badescu <andrei@sealnext.com>
2025-03-30 13:36:40 +0300

    Fixed bugs, now backend starts

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -6,6 +6,14 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+class DBState:
+    count = 0
+
+# Use it like this
+DBState.count += 1
+logger.info(f"count: {DBState.count}")
+
+logger.info(f"DATABASE_URL: {DATABASE_URL}")
 engine = create_async_engine(DATABASE_URL, echo=False)
 
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
