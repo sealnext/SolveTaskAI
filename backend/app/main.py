5a2286415ec0495decd739fb2f11b243c4b83a76
Andy Blake <andy@sealnext.com>
2025-04-19 18:41:42 +0300

    Backend: Improved error handling on middleware request authorization

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,9 +3,9 @@ from contextlib import asynccontextmanager
 from logging import getLogger
 from typing import AsyncGenerator
 
-from fastapi import FastAPI, Request
-from starlette.responses import RedirectResponse
+from fastapi import FastAPI, HTTPException, Request, status
 
+from app.misc.exception import SessionNotFoundException
 from app.misc.pool import db_pool
 from app.misc.postgres import async_db_engine, init_db
 from app.route.agent import router as agent_router
@@ -35,16 +35,16 @@ async def authorize(request: Request, call_next):
 	if request.url.path.startswith('/auth'):
 		return await call_next(request)
 
-	redirect_to_login = RedirectResponse('/auth/login')
-	redirect_to_login.delete_cookie('session_token')
-
 	session_token = request.cookies.get('session_token')
 	if not session_token:
-		return redirect_to_login
+		return HTTPException(status.HTTP_401_UNAUTHORIZED, 'Unauthorized')
 
-	user_id = await AuthService.get_user_id(session_token)
-	if not user_id:
-		return redirect_to_login
+	try:
+		user_id: int = await AuthService.get_user_id(session_token)
+	except SessionNotFoundException:
+		return HTTPException(status.HTTP_401_UNAUTHORIZED, 'Unauthorized')
+	except Exception as e:
+		raise e
 
 	request.state.user_id = user_id
 
