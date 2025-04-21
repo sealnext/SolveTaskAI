bb46ef7a3432cc3d7a8a7a95a21c5cc7e1e54c84
Andrei Badescu <andrei@sealnext.com>
2025-04-21 12:26:11 +0300

    Backend: Fixed middleware bug due to wrong location

--- dev/backend/app/main.py
+++ dev/backend/app/main.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 from logging import getLogger
 from typing import AsyncGenerator
 
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, FastAPI, HTTPException, Request, status
 
 from app.misc.pool import db_pool
 from app.misc.postgres import async_db_engine, init_db
@@ -13,6 +13,8 @@ from app.route.auth import router as auth_router
 from app.route.health import router as health_router
 from app.route.projects import router as projects_router
 from app.route.ticketing import router as ticketing_router
+from app.service.auth import AuthService
+from app.misc.exception import SessionNotFoundException
 
 logger = getLogger(__name__)
 
@@ -27,6 +29,28 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
 
 
 app = FastAPI(lifespan=lifespan)
+
+
+@app.middleware('http')
+async def authorize(request: Request, call_next):
+	if request.url.path.startswith('/api/auth') or request.url.path == '/api/health':
+		return await call_next(request)
+
+	session_token = request.cookies.get('session_token')
+	if not session_token:
+		return HTTPException(status.HTTP_401_UNAUTHORIZED, 'Unauthorized')
+
+	try:
+		user_id: int = await AuthService.get_user_id(session_token)
+	except SessionNotFoundException:
+		return HTTPException(status.HTTP_401_UNAUTHORIZED, 'Unauthorized')
+	except Exception as e:
+		raise e
+
+	request.state.user_id = user_id
+
+	response = await call_next(request)
+	return response
 
 
 app_router = APIRouter()
