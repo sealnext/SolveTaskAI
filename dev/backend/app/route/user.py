419234103004e0c492940077eb69a02e9b16f282
Andrei Badescu <andrei@sealnext.com>
2025-05-08 23:48:07 +0300

    Frontend: Added fetching user's profile in Menu component

--- dev/backend/app/route/user.py
+++ dev/backend/app/route/user.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Request
-
 from app.dependency import UserServiceDep
 from app.dto.user import UserPublic
 
--- dev/frontend/app/components/dropdown-menu.tsx
+++ dev/frontend/app/components/dropdown-menu.tsx
@@ -13,34 +13,107 @@ import {
 	AvatarFallback,
 } from "~/components/ui/avatar"
 import { Form } from "react-router"
-import { Menu as MenuIcon } from "lucide-react"
+import {
+	AlertCircle,
+	Loader2,
+	Menu as MenuIcon,
+	User,
+	Shield,
+	Settings,
+	Users,
+	HelpCircle,
+	LogOut
+} from "lucide-react"
+import { useQuery } from "@tanstack/react-query"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "~/components/ui/tooltip"
 
 export function Menu() {
+
+	const  { isPending, error, data } = useQuery({
+		queryKey: ["user"],
+		queryFn: () => fetch("/api/user/profile")
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(`${response.statusText}`);
+				}
+				return response.json();
+			})
+	});
+
 	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Avatar className="ring-2 ring-offset-1 ring-blue-300 hover:ring-blue-400 data-[state=open]:ring-blue-500 w-10 h-10">
-					<AvatarFallback>
-						<MenuIcon className="h-5 w-5" />
-					</AvatarFallback>
-				</Avatar>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent className="w-40 mt-1 mr-4">
-				<DropdownMenuLabel>My Account</DropdownMenuLabel>
-				<DropdownMenuSeparator />
-				<DropdownMenuGroup>
-					<DropdownMenuItem>Profile</DropdownMenuItem>
-					<DropdownMenuItem>Settings</DropdownMenuItem>
-					<DropdownMenuItem>Invite users via email</DropdownMenuItem>
-					<DropdownMenuItem>Support</DropdownMenuItem>
-				</DropdownMenuGroup>
-				<DropdownMenuSeparator />
-				<DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+		<TooltipProvider>
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Avatar className="ring-2 ring-offset-1 ring-blue-300 hover:ring-blue-400 hover:cursor-pointer data-[state=open]:ring-blue-500 w-10 h-10">
+						<AvatarFallback className="leading-none">
+							{data?.name?.charAt(0).toUpperCase() || data?.email?.charAt(0).toUpperCase() || <MenuIcon className="h-5 w-5" />}
+						</AvatarFallback>
+					</Avatar>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent className={`mt-1 mr-4 ${isPending || error ? 'min-w-[190px]' : 'min-w-[150px]'}`}>
+					<DropdownMenuLabel className="text-center">
+						{isPending ? (
+							<Tooltip>
+								<TooltipTrigger asChild>
+									<div className="flex items-center gap-2">
+										<Loader2 className="h-5 w-5 animate-spin" />
+										Loading...
+									</div>
+								</TooltipTrigger>
+								<TooltipContent>
+									<p>Fetching your name</p>
+								</TooltipContent>
+							</Tooltip>
+						) : error ? (
+							<Tooltip>
+								<TooltipTrigger asChild>
+									<div className="flex items-center gap-2 text-red-600 dark:text-red-400 font-medium cursor-help">
+										<AlertCircle className="h-5 w-5" />
+										{error.message}
+									</div>
+								</TooltipTrigger>
+								<TooltipContent>
+									<p>Failed to fetch your name</p>
+								</TooltipContent>
+							</Tooltip>
+						) : (
+							data?.name || data?.email
+						)}
+					</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuGroup>
+						<DropdownMenuItem className="cursor-pointer">
+							<User className="h-4 w-4 mr-0" />
+							Profile
+						</DropdownMenuItem>
+						<DropdownMenuItem className="cursor-pointer">
+							<Shield className="h-4 w-4 mr-0" />
+							Security
+						</DropdownMenuItem>
+						<DropdownMenuItem className="cursor-pointer">
+							<Settings className="h-4 w-4 mr-0" />
+							Settings
+						</DropdownMenuItem>
+						<DropdownMenuItem className="cursor-pointer">
+							<Users className="h-4 w-4 mr-0" />
+							Invite users
+						</DropdownMenuItem>
+						<DropdownMenuItem className="cursor-pointer">
+							<HelpCircle className="h-4 w-4 mr-0" />
+							Support
+						</DropdownMenuItem>
+					</DropdownMenuGroup>
+					<DropdownMenuSeparator />
 					<Form method="post" className="w-full h-full">
-						<button type="submit" className="w-full h-full text-left">Log out</button>
+						<button type="submit" className="w-full h-full text-left flex items-center gap-2">
+							<DropdownMenuItem onSelect={(e) => e.preventDefault()} className="cursor-pointer w-full h-full">
+								<LogOut className="h-4 w-4" />
+								Log out
+							</DropdownMenuItem>
+						</button>
 					</Form>
-				</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</TooltipProvider>
 	)
 }
